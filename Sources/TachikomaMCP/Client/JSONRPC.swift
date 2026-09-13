10f173bbfc0e90c51b838c43479bfb30127a4356
Peter Steinberger <steipete@gmail.com>
2026-09-12 23:42:23 -0700

    refactor(mcp): share wire envelopes and remove unused client (#91)

---
+++ Sources/TachikomaMCP/Client/JSONRPC.swift
@@ -0,0 +1,61 @@
+struct JSONRPCNotification<P: Encodable>: Encodable {
+    let jsonrpc: String
+    let method: String
+    let params: P
+}
+
+struct JSONRPCResponse<R: Decodable>: Decodable {
+    let jsonrpc: String
+    let result: R?
+    let error: JSONRPCError?
+    let id: JSONRPCID?
+}
+
+enum JSONRPCID: Decodable {
+    case int(Int)
+    case string(String)
+    case null
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        if let i = try? container.decode(Int.self) {
+            self = .int(i)
+            return
+        }
+        if let s = try? container.decode(String.self) {
+            self = .string(s)
+            return
+        }
+        if container.decodeNil() {
+            self = .null
+            return
+        }
+        throw DecodingError.typeMismatch(
+            JSONRPCID.self,
+            .init(codingPath: decoder.codingPath, debugDescription: "Unsupported id type"),
+        )
+    }
+}
+
+struct JSONRPCError: Decodable {
+    let code: Int
+    let message: String
+}
+
+/// Shared JSON-RPC types for HTTP transport
+struct HTTPJSONRPCRequest<P: Encodable>: Encodable {
+    let jsonrpc = "2.0"
+    let method: String
+    let params: P
+    let id: Int
+}
+
+struct HTTPJSONRPCResponse<R: Decodable>: Decodable {
+    let jsonrpc: String
+    let result: R?
+    let error: HTTPJSONRPCError?
+    let id: Int?
+}
+
+struct HTTPJSONRPCError: Decodable { let code: Int
+    let message: String
+}
